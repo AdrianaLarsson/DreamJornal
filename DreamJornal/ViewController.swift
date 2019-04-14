940ebd1aa0585cefcc68d730c3338eaf8277da85
Adriana Pedroza Larsson <larssonadriana@gmail.com>
2019-04-14 08:54:45 +0200

    Initial Commit

---
+++ DreamJornal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DreamJornal
+//
+//  Created by Adriana Pedroza Larsson on 2019-04-14.
+//  Copyright © 2019 Adriana Pedroza Larsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
