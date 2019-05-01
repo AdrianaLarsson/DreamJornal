02f2534201ce5c0c7059c78d9c02c290adb2816f
Adriana Larsson <adrianalarsson@Adrianas-MacBook-Air.local>
2019-05-01 06:31:04 +0200

    first commit

---
+++ DreamJornal/DreamTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DreamTableViewCell.swift
+//  DreamJornal
+//
+//  Created by Adriana Pedroza Larsson on 2019-04-14.
+//  Copyright © 2019 Adriana Pedroza Larsson. All rights reserved.
+//
+
+import UIKit
+
+class DreamTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DreamJornal/EditDreamViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditDreamViewController.swift
+//  DreamJornal
+//
+//  Created by Adriana Pedroza Larsson on 2019-04-14.
+//  Copyright © 2019 Adriana Pedroza Larsson. All rights reserved.
+//
+
+import UIKit
+
+class EditDreamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
